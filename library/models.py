336a902fe4e4f0c929e96aae2547b777d15e253a
Maksim <felozan4ik2007@gmail.com>
2025-05-29 01:15:33 +0300

    ready

--- library/models.py
+++ library/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class LibraryBook(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13, unique=True)
+    available = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+    class Meta:
+        ordering = ['author']
+        indexes = [
+            models.Index(fields=['isbn'], name='isbn_index')
+        ]
